740c7bf65b259d2732311af8e400fd71a4633eea
Adriano Parente <adriano.parente@gmx.de>
2021-08-03 00:00:46 +0200

    array pokemon

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-async function loadPokemon(){
-let url ='https://pokeapi.co/api/v2/pokemon/charmander';
-let response = await fetch(url);
-let responseAsJson = await response.json();
-document.getElementById('name-pokemon').innerHTML = responseAsJson['name'];
-document.getElementById('image-pokemon').src =responseAsJson['sprites']['other']['dream_world']['front_default']
-console.log(responseAsJson);
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon';
+    let response = await fetch(url);
+    let responseAsJson = await response.json();
+    for (let i = 0; i < responseAsJson['results'].length; i++) {
+        let urlX = `https://pokeapi.co/api/v2/pokemon/${responseAsJson['results'][i]['name']}`;
+        let responseX = await fetch(urlX);
+        let responseAsJsonX = await responseX.json();
+        console.log(responseAsJsonX);
+        document.getElementById('cards-pokemon').innerHTML += `
+ <div class="card">
+ <p class="name-pokemon">${responseAsJson['results'][i]['name']}</p>
+ <img class="image-pokemon" src="${responseAsJsonX['sprites']['other']['dream_world']['front_default']}">
+</div>
+ `;
+
+    }
+
+
+    console.log(responseAsJson);
+
 }
